7cada50b456e9b70deaba29b9788ee5f4999b0db
ds4tech <ds4tech@gmail.com>
2021-04-23 12:24:11 +0200

    delete fake posts and add explore function

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,9 @@ from app import db
 from app.forms import RegistrationForm
 from app.forms import LoginForm
 from app.forms import EditProfileForm
+from app.forms import PostForm
+from app.forms import EmptyForm
+from app.models import Post
 from app.models import User
 from datetime import datetime
 
@@ -18,22 +21,19 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
         
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-	user = {'username': "Mat"}
-	posts = [
-		{
-			'author':{'username': 'John'},
-			'body': 'Beatiful day in Portland!'
-		},
-		{
-			'author':{'username':'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-	]
-	return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -117,7 +117,6 @@ def follow(username):
     else:
         return redirect(url_for('index'))
 
-
 @app.route('/unfollow/<username>', methods=['POST'])
 @login_required
 def unfollow(username):
@@ -137,4 +136,8 @@ def unfollow(username):
     else:
         return redirect(url_for('index'))
 
-
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
