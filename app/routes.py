63161e3a9a32af5c040b4eaaf6492f7a3fbeebea
ds4tech <ds4tech@gmail.com>
2021-04-23 14:59:50 +0200

    correct as per doc

--- app/routes.py
+++ app/routes.py
@@ -94,15 +94,14 @@ def register():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('explore', page=posts.next_num) \
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('explore', page=posts.prev_num) \
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
-
     form = EmptyForm()
-    return render_template('user.html', user=user, form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url, form=form)
 
     
 @app.route('/edit_profile', methods=['GET', 'POST'])
